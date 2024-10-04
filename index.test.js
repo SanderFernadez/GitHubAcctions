ff6c5396e3b19ec4f5f57216719e75d3df75f820
Sander Fernandez <sanderfernandez0812@gmail.com>
2024-10-04 19:13:10 -0400

    Nuevo Commit

--- index.test.js
+++ index.test.js
@@ -1,10 +1,6 @@
-
-const sayHello = require('index.js');
+// index.test.js
+const sayHello = require('./index'); // Cambiado a './index'
 
 test('returns "Hola Mundo"', () => {
     expect(sayHello()).toBe("Hola Mundo");
 });
-
-
-
-
